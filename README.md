e91bca92a6c3d82e6d65b0ff2677dfa105c3910d
darren-rose <darrenwrose@gmail.com>
2019-09-13 21:53:00 +0200

    add register login action handlers

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # haskell-rest-cli
+
+setup
+
+```
+stack setup
+```
+
+build
+
+```
+stack build
+```
+
+test
+
+```
+stack test
+```
+
+run register
+
+```
+stack exec haskell-rest-cli register darren pass
+```
+
+run login
+
+```
+stack exec haskell-rest-cli login darren pass
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import System.Environment   
+import System.IO  
+import Data.List  
 
-main :: IO ()
-main = someFunc
+dispatch :: [(String, [String] -> IO ())]  
+dispatch =  [ ("register", register)  
+            , ("login", login)  
+            ]  
+   
+main = do  
+    (command:args) <- getArgs  
+    let (Just action) = lookup command dispatch  
+    action args  
+  
+register :: [String] -> IO ()  
+register [username, password] = putStrLn $ "register " ++ username ++ " " ++ password
+  
+login :: [String] -> IO ()  
+login [username, password] = do  
+    putStrLn $ "login " ++ username ++ " " ++ password
